df54b2f1f349cc467cde707658696c9937f8e7d1
Imartino4 <ignacio.martacos@gmail.com>
2022-12-20 18:11:39 +0000

    Task 0

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+window.onload = function () {
+    const checkbox = document.querySelector('input[type=checkbox]')
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            const a_li = document.querySelector("amenities li");
+            const a_id = a_li.getAttribute(':amenity_id');
+            const dir_am = {'amenity_id'}}
+        }
+    ) }
